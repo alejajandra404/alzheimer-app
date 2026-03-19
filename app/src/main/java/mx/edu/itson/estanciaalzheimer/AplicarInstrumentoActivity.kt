8f1885d849ca1df594def52b91e583852ca61494
alejandra <168584883+alejajandra404@users.noreply.github.com>
2026-03-18 20:30:58 -0700

    add empty activities and layouts for all screens

---
+++ app/src/main/java/mx/edu/itson/estanciaalzheimer/AplicarInstrumentoActivity.kt
@@ -0,0 +1,11 @@
+package mx.edu.itson.estanciaalzheimer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AplicarInstrumentoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_aplicar_instrumento)
+    }
+}
---
+++ app/src/main/java/mx/edu/itson/estanciaalzheimer/ResultadosActivity.kt
@@ -0,0 +1,11 @@
+package mx.edu.itson.estanciaalzheimer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ResultadosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resultados)
+    }
+}
